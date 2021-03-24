4b9d773a9ec3df3d2eca9aa9796d6677cca6ed9d
Daniel <daniel.fb.jr@gmail.com>
2021-03-24 20:09:35 -0300

    Cartas

--- BlackJack/Form1.Designer.cs
+++ BlackJack/Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace BlackJack
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(413, 488);
+            this.Name = "Form1";
+            this.Text = "Black Jack (Versão1.0)";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
